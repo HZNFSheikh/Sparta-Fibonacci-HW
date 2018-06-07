bbc80d75eec83190c62600db62221f4652311ee8
HZNFSheikh <hsheikh@spartaglobal.com>
2018-06-07 05:44:05 +0100

    HW attempt

---
+++ libs/fibonacci.rb
@@ -0,0 +1,19 @@
+class Fibonacci
+
+  class << self
+    include Enumerable
+
+    def each
+      return to_enum unless block_given?
+      a = 0; b = 1
+      loop do
+        a, b = b, a + b
+        yield a
+      end
+    end
+  end
+end
+
+puts Fibonacci.take_while { |i| i < 4000000 }.
+          select(&:even?).
+          inject(:+)
